68eafddb15fd333884cfc0218ad3f7cb9ab27d5c
Aleynapuppy <102704129+Aleynapuppy@users.noreply.github.com>
2022-10-03 20:06:06 -0400

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -33,9 +33,9 @@ like=message_data['like'];
 name_with_tag="<h4>"+name+"<img class='user_tick' src='tick.png'></h4>";
 message_with_tag="<h4 class='meassge_h4'>"+message+"</h4>";
 like_button="<button class='btn btn-warning'id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
-span_with_tag="<span class='glyphicon glyphicon-thumb-up'> Like:"+like+"</span></button><hr>";
+span_with_tag="<span class='glyphicon glyphicon-thumbs-up'> Like:"+like+"</span></button><hr>";
 row=name_with_tag+message_with_tag+like_button+span_with_tag;
-document.getElemetById("output").innerhtml+=row;
+document.getElementById("output").innerHTML+=row;
 
 //End code
       } });  }); }
@@ -46,7 +46,7 @@ function updateLike(message_id){
       button_id=message_id;
       likes=document.getElementById(button_id).value;
       updated_likes=Number(likes)+1;
-      comsole.log(updated_likes);
+      console.log(updated_likes);
       firebase.database().ref(room_name).child(message_id).update({
             like:updated_likes
       });
